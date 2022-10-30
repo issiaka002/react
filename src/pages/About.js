734434e65509944d70ad68f88fae18c2a4d2ceff
rill <rill88@gmail.com>
2022-10-30 19:19:13 +0000

    create the navigation page

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+      render() {
+            return (
+                  <div>
+                        <div>
+                              <h1>
+                                    About
+                              </h1>
+                              <p>
+                                    a propos du site
+                              </p>
+                        </div>
+                  </div>
+            );
+      }
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+      render() {
+            return (
+                  <div>
+                        <div>
+                              <h1>
+                                    ACCEUIL 
+                                    <p>
+                                          Bienvenue sur mon site ...
+                                    </p>
+                              </h1>
+                        </div>
+                  </div>
+            );
+      }
+}
+
+export default Home;
